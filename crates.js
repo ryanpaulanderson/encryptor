d3a281b1e49b0e6b1a67a27efb8487484e1e34d2
ryanpaulanderson <ryanpaulanderson@users.noreply.github.com>
2025-05-20 18:53:54 +0000

    deploy: 52b92e35abfa5bf1ea5451744fdf06042a672df5

--- crates.js
+++
@@ -1,2 +0,0 @@
-window.ALL_CRATES = ["chacha20_poly1305","encryptor"];
-//{"start":21,"fragment_lengths":[19,12]}
--- trait.impl/core/error/trait.Error.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["encryptor",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"encryptor/error/enum.Error.html\" title=\"enum encryptor::error::Error\">Error</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[270]}
