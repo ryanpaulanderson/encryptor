27e4ae347f0bd0a1ca07993f68bc5ea97a58b7cf
ryanpaulanderson <27966966+ryanpaulanderson@users.noreply.github.com>
2025-05-21 12:42:13 -0400

    Remove Secret wrapper and use SecretBox

--- tests/chacha20_vectors.rs
+++ tests/chacha20_vectors.rs
@@ -1,12 +1,12 @@
 use encryptor::chacha20_block;
-use secrecy::Secret;
+use secrecy::SecretBox;
 
 #[test]
 fn rfc8439_block0() {
     // Test vector from RFC 8439 \u00a72.3.2
     let key = [0u8; 32];
     let nonce = [0u8; 12];
-    let block = chacha20_block(&Secret::new(key), 1, &nonce);
+    let block = chacha20_block(&SecretBox::new(Box::new(key)), 1, &nonce);
     let expected = hex::decode(
         "9f07e7be5551387a98ba977c732d080dcb0f29a048e3656912c6533e32ee7aed\
          29b721769ce64e43d57133b074d839d531ed1f28510afb45ace10a1f4b794d6f",
@@ -18,11 +18,11 @@ fn rfc8439_block0() {
 #[test]
 fn rfc8439_block1() {
     // RFC 8439 \u00a72.3.2 test vector: counter=1, key=0..31
-    let key = Secret::new([
+    let key = SecretBox::new(Box::new([
         0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
         0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d,
         0x1e, 0x1f,
-    ]);
+    ]));
     let nonce = [
         0x00, 0x00, 0x00, 0x09, 0x00, 0x00, 0x00, 0x4a, 0x00, 0x00, 0x00, 0x00,
     ];
